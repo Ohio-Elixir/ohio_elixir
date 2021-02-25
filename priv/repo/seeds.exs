7ca1465dbf8570eecca7bff8ffa6784e0808843d
Parker Bartlett <prbartlett1@gmail.com>
2021-02-24 22:53:35 -0500

    create some seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,39 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias OhioElixir.Repo
+alias OhioElixir.Events
+alias OhioElixir.Events.Meeting
+alias OhioElixir.Events.Speaker
+
+meeting_one =
+  Repo.insert!(%Meeting{
+    title: "A really neat LiveView talk",
+    date: DateTime.utc_now() |> DateTime.truncate(:second)
+  })
+  |> Repo.preload(:speakers)
+
+meeting_two =
+  Repo.insert!(%Meeting{
+    title: "The coolest Nerves presentation",
+    date: DateTime.utc_now() |> DateTime.truncate(:second)
+  })
+  |> Repo.preload(:speakers)
+
+speaker_one =
+  Repo.insert!(%Speaker{
+    name: "Jane Doe",
+    github_url: "github.com/janedoe",
+    twitter_url: "twitter.com/janedoe"
+  })
+
+speaker_two =
+  Repo.insert!(%Speaker{
+    name: "John Doe",
+    github_url: "github.com/johndoe",
+    twitter_url: "twitter.com/johndoe"
+  })
+
+Events.add_speaker_to_meeting(meeting_one, speaker_one)
+Events.add_speaker_to_meeting(meeting_two, speaker_two)
