908a08a4884039284fc5b13336110e90bd647e95
Parker Bartlett <prbartlett1@gmail.com>
2022-03-11 20:32:19 -0500

    update elixir/erlang versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule OhioElixir.MixProject do
     [
       app: :ohio_elixir,
       version: "0.1.0",
-      elixir: "~> 1.12.0",
+      elixir: "~> 1.13.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
