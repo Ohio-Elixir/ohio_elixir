be1f41a694275689be8c8eceef6149751479fb86
Tim Mecklem <timothy@mecklem.com>
2022-07-23 11:17:20 -0400

    Remove compiler warning about get text no longer being needed in list

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule OhioElixir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13.3",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
