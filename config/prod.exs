c92c45c7b2c3e4a4937f99c474903453bce95e02
Tim Mecklem <timothy@mecklem.com>
2022-07-24 22:22:56 -0400

    Mix format :shakes fist:

--- config/prod.exs
+++ config/prod.exs
@@ -63,8 +63,7 @@ config :ohio_elixir, OhioElixir.Repo,
 # We also recommend setting `force_ssl` in your endpoint, ensuring
 # no data is ever sent via http, always redirecting to https:
 #
-config :ohio_elixir, OhioElixirWeb.Endpoint,
-  force_ssl: [hsts: true]
+config :ohio_elixir, OhioElixirWeb.Endpoint, force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
 
