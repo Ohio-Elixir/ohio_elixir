7081eb776f09dfdec0935444b16e3e3af9560c50
Parker Bartlett <prbartlett1@gmail.com>
2023-10-01 23:03:44 -0400

    change force ssl config

--- config/prod.exs
+++ config/prod.exs
@@ -63,7 +63,8 @@ config :ohio_elixir, OhioElixir.Repo,
 # We also recommend setting `force_ssl` in your endpoint, ensuring
 # no data is ever sent via http, always redirecting to https:
 #
-config :ohio_elixir, OhioElixirWeb.Endpoint, force_ssl: [hsts: true]
+config :ohio_elixir, OhioElixirWeb.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_proto]]
+
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
 
